598471e629bfa2e9f40ecf7354632e06d4a26115
DavidVelosoduocuc <dav.veloso@duocuc.cl>
2026-04-24 12:49:18 -0400

    primer commit de ms gestion

---
+++ src/test/java/cl/rednorte/ms_registro/MsRegistroApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.rednorte.ms_registro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegistroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
