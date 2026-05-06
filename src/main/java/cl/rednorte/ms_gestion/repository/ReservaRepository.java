93308b43abb902e018ff85243da9273e94f91d94
Oscar Garrido <osca.garrido@duocuc.cl>
2026-05-06 16:27:34 -0400

    feat: añadimos metricas para las funciones de la vista de director

--- src/main/java/cl/rednorte/ms_gestion/repository/ReservaRepository.java
+++ src/main/java/cl/rednorte/ms_gestion/repository/ReservaRepository.java
@@ -2,6 +2,8 @@ package cl.rednorte.ms_gestion.repository;
 
 import cl.rednorte.ms_gestion.entity.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +14,7 @@ public interface ReservaRepository extends JpaRepository<Reserva, Long> {
     List<Reserva> findByPacienteId(Long pacienteId);
     List<Reserva> findByCentroId(Long centroId);
     List<Reserva> findByMedicoId(Long medicoId); 
+    
+    @Query(value = "SELECT count(*) FROM reserva WHERE CAST(estado AS text) = :#{#estado.name()}", nativeQuery = true)
+    long countByEstado(@Param("estado") Reserva.EstadoReserva estado);
 }
