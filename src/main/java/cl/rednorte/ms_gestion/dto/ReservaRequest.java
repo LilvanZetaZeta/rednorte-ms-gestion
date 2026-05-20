3e3b1b8183284fcd2a10ab5c31d5b41a37850c8d
LilvanZetaZeta <iv.zamoranov@duocuc.cl>
2026-05-20 15:32:31 -0400

    Modificaciones para que secretaria pueda generar la reserva

--- src/main/java/cl/rednorte/ms_gestion/dto/ReservaRequest.java
+++ src/main/java/cl/rednorte/ms_gestion/dto/ReservaRequest.java
@@ -3,18 +3,29 @@ package cl.rednorte.ms_gestion.dto;
 import java.time.LocalDateTime;
 
 import cl.rednorte.ms_gestion.entity.Reserva.OrigenReserva;
+import cl.rednorte.ms_gestion.validation.ValidRut;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class ReservaRequest {
-    @NotNull(message = "El ID del paciente es requerido")
+    
+    // Ahora es opcional porque puede venir solo el RUT si es paciente nuevo
     private Long pacienteId;
+    
+    @ValidRut
+    private String pacienteRut;
+    private String pacienteCorreo;
+    private String pacienteNombreCompleto;
+
     @NotNull(message = "El ID del médico es requerido")
     private Long medicoId;
+    
     @NotNull(message = "El ID del centro es requerido")
     private Long centroId;
+    
     @NotNull(message = "La fecha y hora son requeridas")
     private LocalDateTime fechaHora;
+    
     private OrigenReserva origen = OrigenReserva.WEB;
 }
